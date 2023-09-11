887b12554657213a4558932416d289ea44f14a79
camille <106037608+rirum@users.noreply.github.com>
2023-09-11 17:21:09 -0300

    fix: small fix on validation / prisma migration

--- src/schema/floss-schema.ts
+++ src/schema/floss-schema.ts
@@ -5,6 +5,8 @@ export const flossSchema = Joi.object<FlossType>({
   code: Joi.string().required(),
   brandName: Joi.string().min(3).required(),
   flossId: Joi.string().required(),
+  flossImg: Joi.string(),
+  description: Joi.string(),
   red: Joi.number(),
   blue: Joi.number(),
   green: Joi.number(),
